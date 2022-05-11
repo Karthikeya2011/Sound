1eee47a7187eab9af1e5f16f788ec4662d277452
Karthikeya2011 <90773593+Karthikeya2011@users.noreply.github.com>
2022-05-11 17:27:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classification = ml5.surroundClassifier('https://teachablemachine.withgoogle.com/models/[...]model.json',modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResult);
+}
